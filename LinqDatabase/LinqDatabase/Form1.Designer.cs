6441a686f98de0e28eb006fb24aee53e8d6befb5
JohnVanSchalkwyk <johnvanschalkwyk@gmail.com>
2017-04-20 08:25:34 +0200

    Added interface structure

--- LinqDatabase/LinqDatabase/Form1.Designer.cs
+++ LinqDatabase/LinqDatabase/Form1.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnStaff = new System.Windows.Forms.Button();
+            this.btnFleet = new System.Windows.Forms.Button();
+            this.btnClients = new System.Windows.Forms.Button();
+            this.btnBookings = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(476, 333);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Controls.Add(this.btnStaff);
+            this.panel2.Location = new System.Drawing.Point(241, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(232, 160);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel3.Controls.Add(this.btnFleet);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(232, 160);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel4.Controls.Add(this.btnBookings);
+            this.panel4.Location = new System.Drawing.Point(3, 169);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(232, 160);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel5.Controls.Add(this.btnClients);
+            this.panel5.Location = new System.Drawing.Point(241, 169);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(232, 160);
+            this.panel5.TabIndex = 2;
+            // 
+            // btnStaff
+            // 
+            this.btnStaff.Location = new System.Drawing.Point(3, 3);
+            this.btnStaff.Name = "btnStaff";
+            this.btnStaff.Size = new System.Drawing.Size(226, 154);
+            this.btnStaff.TabIndex = 0;
+            this.btnStaff.Text = "Manage Staff";
+            this.btnStaff.UseVisualStyleBackColor = true;
+            this.btnStaff.Click += new System.EventHandler(this.btnStaff_Click);
+            // 
+            // btnFleet
+            // 
+            this.btnFleet.Location = new System.Drawing.Point(3, 3);
+            this.btnFleet.Name = "btnFleet";
+            this.btnFleet.Size = new System.Drawing.Size(226, 154);
+            this.btnFleet.TabIndex = 1;
+            this.btnFleet.Text = "Manage Fleet";
+            this.btnFleet.UseVisualStyleBackColor = true;
+            this.btnFleet.Click += new System.EventHandler(this.btnFleet_Click);
+            // 
+            // btnClients
+            // 
+            this.btnClients.Location = new System.Drawing.Point(3, 3);
+            this.btnClients.Name = "btnClients";
+            this.btnClients.Size = new System.Drawing.Size(226, 154);
+            this.btnClients.TabIndex = 1;
+            this.btnClients.Text = "Manage Clients";
+            this.btnClients.UseVisualStyleBackColor = true;
+            this.btnClients.Click += new System.EventHandler(this.btnClients_Click);
+            // 
+            // btnBookings
+            // 
+            this.btnBookings.Location = new System.Drawing.Point(3, 3);
+            this.btnBookings.Name = "btnBookings";
+            this.btnBookings.Size = new System.Drawing.Size(226, 154);
+            this.btnBookings.TabIndex = 2;
+            this.btnBookings.Text = "Manage Bookings";
+            this.btnBookings.UseVisualStyleBackColor = true;
+            this.btnBookings.Click += new System.EventHandler(this.btnBookings_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(476, 333);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Main Menu - Welcome Back";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnStaff;
+        private System.Windows.Forms.Button btnFleet;
+        private System.Windows.Forms.Button btnClients;
+        private System.Windows.Forms.Button btnBookings;
     }
 }
 
--- LinqDatabase/LinqDatabase/Form1.cs
+++ LinqDatabase/LinqDatabase/Form1.cs
@@ -16,5 +16,39 @@ namespace LinqDatabase
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnFleet_Click(object sender, EventArgs e)
+        {
+            FleetManagement fm = new FleetManagement();
+            fm.ShowDialog();
+        }
+
+        private void btnStaff_Click(object sender, EventArgs e)
+        {
+            StaffManagement sm = new StaffManagement();
+            sm.ShowDialog();
+        }
+
+        private void btnBookings_Click(object sender, EventArgs e)
+        {
+            Bookings b = new Bookings();
+            b.ShowDialog();
+        }
+
+        private void btnClients_Click(object sender, EventArgs e)
+        {
+            ClientManagement cm = new ClientManagement();
+            cm.ShowDialog();
+        }
     }
 }
